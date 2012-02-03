c1a01b3532ce512160e4038aaaa55c51f6d4cd8b
David Haws <dchaws@gmail.com>
2012-02-02 19:58:16 -0500

    First commit

---
+++ genbintrees.cpp
@@ -0,0 +1,13 @@
+// David C. Haws 2/2/2012
+// Program to generate all binary trees with labeled leafs.
+
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    cout << "This program will generate all labeled binary trees." << endl;
+}
